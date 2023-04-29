eade5ece9ec074f87dbafef22526f1418134f4e4
Mardian2507 <mardian.2507@gmail.com>
2023-04-30 01:16:30 +0700

    delete counter visitor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,9 @@
-import React from "react";
-import CountVisit from "./CounVisit";
-
 export default function Footer() {
   return (
     <>
       <footer className="fixed bottom-0 w-3/4">
         <div className="flex justify-center p-2">
-          <CountVisit apiUrl="https://api.countapi.xyz/update/localhost/8d6b22d7-bc89-4997-8ea7-855985fd39b1/?amount=1" />
+          <h3>copyright 2023 Mar-Dian</h3>
         </div>
       </footer>
     </>
